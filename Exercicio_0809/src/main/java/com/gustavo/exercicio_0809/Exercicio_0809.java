2f7bd2cbef2c344383e38b2962e6fc4a85ec6357
xviigimenez <gimenez@tuta.io>
2024-09-06 10:47:14 -0300

    Criado o projeto para a tarefa de 08/09

---
+++ Exercicio_0809/src/main/java/com/gustavo/exercicio_0809/Exercicio_0809.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gustavo.exercicio_0809;
+
+/**
+ *
+ * @author gustavo.correa
+ */
+public class Exercicio_0809 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
